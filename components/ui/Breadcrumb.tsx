15d06885a26e074dff59cde24cf12a197b0994c4
Bruno Rodrigues <bruno@agenciaeplus.com.br>
2023-12-27 02:05:15 -0300

    plp breadcrumb & title done

--- components/ui/Breadcrumb.tsx
+++ components/ui/Breadcrumb.tsx
@@ -8,16 +8,16 @@ function Breadcrumb({ itemListElement = [] }: Props) {
   const items = [{ name: "Home", item: "/" }, ...itemListElement];
 
   return (
-    <div class="w-full max-w-[96rem] mx-auto flex p-[15px] font-montserrat text-xs font-medium leading-[1.15]">
-      <ul>
+    <div class="w-full max-w-[96rem] sm:px-[5vw] mx-auto p-[15px] font-montserrat text-xs font-medium leading-[1.15]">
+      <ul class="flex">
         {items
           .filter(({ name, item }) => name && item)
           .map(({ name, item }) => (
             <li
               data-highlight={name === "Home"}
-              class="data-[highlight='true']:text-primary-500 data-[highlight='true']:font-bold leading-[1.15] px-1 py-[2px]"
+              class="data-[highlight='true']:text-primary-500 data-[highlight='true']:font-bold leading-[1.15] [&:first-child]:px-0 [&:not(:first-child)]:before:content-['/'] px-1"
             >
-              <a href={item}>{name}</a>
+              <a class="px-1 py-[2px]" href={item}>{name}</a>
             </li>
           ))}
       </ul>
---
+++ sections/Search/PLPTitle.tsx
@@ -0,0 +1,15 @@
+import { BreadcrumbListLoader } from "$store/loaders/breadcrumb.ts";
+
+export interface Props {
+  data: BreadcrumbListLoader;
+}
+
+export default function PLPTitle({ data }: Props) {
+  const title = data.itemListElement.pop()?.name;
+
+  return (
+    <h1 class="w-full max-w-[96rem] mx-auto my-[1em] font-montserrat text-2xl !leading-[1.15] font-semibold px-2 sm:px-[5vw] text-black">
+      {title}
+    </h1>
+  );
+}

46adc74bb03ee9f2cd6ff4f2d2fea701c24fd957
Saymon Damásio <saymon.damasio95@gmail.com>
2024-02-26 18:32:43 -0300

    fix: login header

--- components/header/Main.tsx
+++ components/header/Main.tsx
@@ -11,6 +11,7 @@ import Searchbar from "$store/islands/Header/Searchbar.tsx";
 import { usePlatform } from "$store/sdk/usePlatform.tsx";
 import Image from "apps/website/components/Image.tsx";
 import { navbarHeight } from "./constants.ts";
+import { useUser } from "deco-sites/std/packs/vtex/hooks/useUser.ts";
 
 function Main({ searchbar, logo, isMobile }: {
   searchbar?: SearchbarProps;
@@ -18,6 +19,8 @@ function Main({ searchbar, logo, isMobile }: {
   isMobile?: boolean;
 }) {
   const platform = usePlatform();
+  const { user } = useUser();
+  const isLoggedIn = !!user.value?.email;
 
   return (
     <>
@@ -73,26 +76,53 @@ function Main({ searchbar, logo, isMobile }: {
         </div>
         <Searchbar searchbar={searchbar} />
         <div class="flex-none items-center hidden lg:flex">
-          <a
-            class="flex items-center "
-            href="/login"
-            aria-label="Log in"
-          >
-            <div class="flex items-center gap-2 group-data-[micro-header='true']/header:justify-between group-data-[micro-header='true']/header:flex-col transition-all h-[48px]">
-              <Icon
-                id="User"
-                size={20}
-                strokeWidth={0}
-                class="text-primary-500"
-              />
+          <div class="flex items-center gap-2 group group-data-[micro-header='true']/header:justify-between group-data-[micro-header='true']/header:flex-col transition-all h-[48px] relative">
+            <Icon
+              id="User"
+              size={20}
+              strokeWidth={0}
+              class="text-primary-500"
+            />
+            <a
+              class="flex items-center "
+              href="/login"
+              aria-label="Log in"
+            >
               <span class="block uppercase text-xs font-bold group-data-[micro-header='true']/header:hidden">
-                Faça Login | Cadastre-se
+                {isLoggedIn ? `Olá, ${user.value?.email}` : (
+                  <>
+                    Faça Login | Cadastre-se
+                  </>
+                )}
               </span>
               <span class="hidden text-xs group-data-[micro-header='true']/header:block leading-none">
                 Entrar
               </span>
-            </div>
-          </a>
+            </a>
+            {isLoggedIn && (
+              <>
+                <div class="group-hover:flex hidden -translate-x-1/2 left-1/2 absolute  
+              w-[2rem] h-[2rem] rotate-45 shadow-[4px_8px_8px_rgba(0,0,0,.25)] top-[34px]">
+                </div>
+                <ul class="group-hover:flex hidden -translate-x-1/2 left-1/2 top-full absolute bottom-[-20px] bg-white shadow-[4px_8px_8px_rgba(0,0,0,.25)] min-h-[170px] min-w-[300px] flex-col justify-center items-center z-50">
+                  <li>
+                    <a href="/account">Minha conta</a>
+                  </li>
+                  <li>
+                    <a href="/account#/orders">Meus pedidos</a>
+                  </li>
+                  <li>
+                    <a
+                      class="block px-4 py-[10px] text-center text-danger"
+                      href={`/api/vtexid/pub/logout?scope=atacadaosexyshop&returnUrl=${window.location?.href}`}
+                    >
+                      Sair
+                    </a>
+                  </li>
+                </ul>
+              </>
+            )}
+          </div>
         </div>
         <div class="flex-1 lg:flex-none flex items-center lg:justify-start justify-end">
           {platform === "vtex" && <CartButtonVTEX />}

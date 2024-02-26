c5e736fe9c253fd0b7001ab2fd730ff1797bce55
Bruno Rodrigues <bruno@agenciaeplus.com.br>
2024-02-26 10:02:26 -0300

    add build target to fresh config

--- fresh.config.ts
+++ fresh.config.ts
@@ -3,5 +3,6 @@ import plugins from "https://denopkg.com/deco-sites/std@1.22.0/plugins/mod.ts";
 import manifest from "./manifest.gen.ts";
 
 export default defineConfig({
+  build: { target: ["chrome99", "safari99", "safari12"] },
   plugins: plugins({ manifest }),
 });
